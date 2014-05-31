01185656e6ae4c3a4b4a83b5e58cb51ce0f9609e
abnun <abnun@gmx.de>
2014-05-31 11:24:49 +0200

    - Englische Version gelöscht

--- en/header.php
+++
@@ -1,3 +0,0 @@
-<div id="header_en">
-    <a href="startseite.php" title="Startseite"><img src="../images/symbol_auf_farbe_rgb_100px.gif" alt="Startseite" style="border: 0;"/></a>
-</div>
--- en/language_switch.php
+++
@@ -1,6 +0,0 @@
-<div id="language_switch">
-    <ul>
-        <li><a href="#">english</a></li>
-        <li id="skip"><a href="../deutsch/startseite.php">deutsch</a></li>
-    </ul>
-</div>
